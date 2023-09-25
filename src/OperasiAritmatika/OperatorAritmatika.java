990a112a0f253c457b0966c7830b316fd981f13e
Muhammad Bintang Mahardani <bodoybahrudin@gmail.com>
2023-09-25 21:54:43 +0700

    firs commit

---
+++ src/OperasiAritmatika/OperatorAritmatika.java
@@ -0,0 +1,31 @@
+package OperasiAritmatika;
+
+public class OperatorAritmatika {
+public static void main ( String [] args) 
+{
+// Deklarasi dan inisialisasi variabel
+int i = 9 ;
+int j = 4 ;
+
+// Menampilkan nilai varibel
+System.out.println ( " Nilai variabel : " ) ;
+System.out.println (" i = " + i ) ;
+System.out.println (" j = " + j) ;
+
+// Menggunakan operator penjumlahan
+System.out.println ( " Penjumlahan : " ) ;
+System.out.println (" i + j = " + ( i + j ));
+// Menggunakan Operator pengurangan
+System.out.println ("Pengurangan : " ) ;
+System.out.println (" i + j = " + ( i - j));
+// Menggunakan Operator perkalian
+System.out.println (" Perkalian : " );
+System.out.println (" i * j = " + ( i * j));
+// Menggunakan operator pembagian
+System.out.println ( " Pembagian ; " );
+System.out.println (" i / j = " + ( i / j ));
+// Menggunakan operator modulus
+System.out.println ( " Modulus :" ) ;
+System.out.println ( " i % j = " + ( i % j)) ;
+}
+}
