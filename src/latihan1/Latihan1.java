46a5ba30d7c9c0511cc13758d7ad24a189643e79
Muhammad Bintang Mahardani <bodoybahrudin@gmail.com>
2023-09-25 22:28:15 +0700

    firs commit

--- src/latihan1/Latihan1.java
+++ src/latihan1/Latihan1.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package latihan1;
 
-/**
- *
- * @author CYBER
- */
-public class Latihan1 {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Latihan1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Selamat Belajar Java Programing");
